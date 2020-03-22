da3c30ade1413db2eb03a901a28bd86e85bc6abe
angela <angela@aviak.ca>
2020-03-22 08:47:53 -0600

    minimal auth0 code

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+// src/components/NavBar.js
+/*************************
+ * shows login and logout buttons
+ */
+
+import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+    </div>
+  );
+};
+
+export default NavBar;
